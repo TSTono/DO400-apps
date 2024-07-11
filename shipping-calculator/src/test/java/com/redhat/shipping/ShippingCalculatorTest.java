e17acce71c71260db7cca004b9847d2e32f5d61a
qgftmo <your@user.email>
2024-07-11 03:45:56 -0400

    Replace Jenkinsfile

--- shipping-calculator/src/test/java/com/redhat/shipping/ShippingCalculatorTest.java
+++ shipping-calculator/src/test/java/com/redhat/shipping/ShippingCalculatorTest.java
@@ -4,7 +4,43 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.beans.Transient;
+
 public class ShippingCalculatorTest {
 
     // @todo: add tests
+    @Transient
+    public void canCalculateTheCostForARegion() {
+        ShippingCalculator calculator = new ShippingCalculator();
+        assertEquals(0, calculator.costForRegion("A Region"));
+    }
+
+    // @Transient
+    // public void onNARegionTheCostIs100() {
+    //     assertEquals(100, (new ShippingCalculator()).costForRegion("NA"));
+    // }
+
+    @Transient
+    public void onNARegionTheCostIs100() {
+        // Given a shipping calculator
+        ShippingCalculator calculator = new ShippingCalculator();
+
+        // When NA is the country region
+        int calculatedCost = calculator.costForRegion("NA");
+
+        // Then the shipping cost is 100
+        assertEquals(100, calculatedCost);
+    }
+
+    @Transient
+    public void onLATAMRegionTheCostIs200() {
+        // Given a shipping calculator
+        ShippingCalculator calculator = new ShippingCalculator();
+
+        // When LATAM is the country region
+        int calculatedCost = calculator.costForRegion("LATAM");
+
+        // Then the shipping cost is 200
+        assertEquals(200, calculatedCost);
+    }
 }
